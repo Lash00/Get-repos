9f9dd13928bbe285d8c8e8ac13e6e92b99cdfa46
Lash00 <mocotign@gmail.com>
2024-09-07 09:15:54 +0300

    -Legndry Up date the Logic and some coller

--- lol.js
+++ lol.js
@@ -1,11 +1,13 @@
 let containerNames=document.querySelector(".right");
 console.log("السلام عليكم ورحمة الله وبركاته");
 let nameOfrepos=[];
+let na='';
 let button=document.querySelector(".btn");
 // https://api.github.com/users/octocat/repos
 button.addEventListener("click",function()    {   
     containerNames.innerHTML = ''; 
     const name=document.getElementById("name").value;
+    na=name;
     console.log(name);
     let text=`https://api.github.com/users/${name}/repos`;
     console.log(text);
@@ -25,20 +27,29 @@ fetch(text)
       })
       .then(print => {
         print.forEach(element => {
-            nameOfrepos.push(element.name);
+           // nameOfrepos.push(element.name);
+           let div=document.createElement("div");
+           let a=document.createElement("a");
+           div.classList="black";
+           let repoName=document.createTextNode(element.name);
+           a.appendChild(repoName)
+           div.appendChild(a);
+           a.href=`https://github.com/${name}/${element.name}`;
+           a.setAttribute('target','_blank');
+           containerNames.appendChild(div);  
         });
     })
-      .then(print => {
-        if(nameOfrepos.length===0)
-        {
-          create("U dont Have any repos yet......");
-        }
-        else {
-          nameOfrepos.forEach(element => {
-            create(element);
-          });
-        }
-    });
+    //   .then(print => {
+    //     if(nameOfrepos.length===0)
+    //     {
+    //       create("U dont Have any repos yet......");
+    //     }
+    //     else {
+    //       nameOfrepos.forEach(element => {
+    //         create(element);
+    //       });
+    //     }
+    // });
     document.getElementById("name").value="";
     nameOfrepos=[];
 
@@ -46,9 +57,9 @@ fetch(text)
 }
 );
 
-function create(el)
+ function create(el)
 {
-   
+   // set the logic massage 
     let div=document.createElement("div");
     div.classList="black";
     let repoName=document.createTextNode(el);
